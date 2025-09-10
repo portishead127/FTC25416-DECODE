34f1a9674e874f0abfdaea094844b6bf72caee61
unjty <nathanlimurray2@gmail.com>
2025-09-10 12:59:37 +0100

    Started adding Motor Test Class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/tests/MotorTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.FSL.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Test: Motor Test", group = "Test")
+public class MotorTest extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
