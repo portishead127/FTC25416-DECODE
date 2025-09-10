672d75933165aea8dc6bb763e7d7ff7211e0b8bc
unjty <nathanlimurray2@gmail.com>
2025-09-10 15:23:07 +0100

    Completed Motor Test Class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/tests/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/tests/MotorTest.java
@@ -2,12 +2,33 @@ package org.firstinspires.ftc.teamcode.FSL.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp(name = "Test: Motor Test", group = "Test")
 public class MotorTest extends LinearOpMode
 {
+    DcMotor testMotor;
     @Override
     public void runOpMode() throws InterruptedException {
+        testMotor = hardwareMap.get(DcMotorEx.class, "test");
+
+        waitForStart();
+
+        double currentPower = 0.0;
+        double storedPower = 0.0;
+
+        while(opModeIsActive()){
+
+            if(gamepad1.dpad_up) currentPower += 0.1;
+            if(gamepad1.dpad_down) currentPower -= 0.1;
+            if(gamepad1.dpad_left) currentPower += 0.05;
+            if(gamepad1.dpad_right) currentPower -= 0.05;
+            if(gamepad1.circle) storedPower = currentPower;
+            if(gamepad1.triangle) currentPower = storedPower;
+
+            testMotor.setPower(currentPower);
+        }
 
     }
 }
