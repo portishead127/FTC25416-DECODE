1fb14ace557207f19a88e19820d9ca9d634f10a2
Aaron Portis <aaronjportis@gmail.com>
2025-09-17 17:01:55 +0100

    Added motors to motor group in shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/Shooter.java
@@ -4,29 +4,24 @@ package org.firstinspires.ftc.teamcode.FSL.helper.subsystems;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.MotorGroup;
 import dev.nextftc.hardware.impl.MotorEx;
 import dev.nextftc.hardware.powerable.SetPower;
 
 public class Shooter implements Subsystem {
     public static Shooter INSTANCE = new Shooter();
     private Shooter(){}
-    private final MotorEx shooterMotor1 = new MotorEx("SM1")
-            .reversed()
-            .brakeMode();
-    private final MotorEx shooterMotor2 = new MotorEx("SM2")
-            .reversed()
-            .brakeMode();
 
-    public Command fire = new ParallelGroup(
-            new SetPower(shooterMotor1,1),
-            new SetPower(shooterMotor2, 1)
+    private final MotorGroup shooterMotors = new MotorGroup(
+            new MotorEx("SM1")
+                    .reversed()
+                    .brakeMode(),
+            new MotorEx("SM2")
+                    .reversed()
+                    .brakeMode()
     );
-    public Command fireHalf = new ParallelGroup(
-            new SetPower(shooterMotor1,0.5),
-            new SetPower(shooterMotor2, 0.5)
-    );
-    public Command stop = new ParallelGroup(
-            new SetPower(shooterMotor1,0),
-            new SetPower(shooterMotor2, 0)
-    );
+
+    public Command fire = new SetPower(shooterMotors,1);
+    public Command fireHalf = new SetPower(shooterMotors, 0.5);
+    public Command stop = new SetPower(shooterMotors, 0);
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.md
@@ -4,5 +4,5 @@
 | backRightMotor  | REV Ultraplanetary Motor | BRM         | CH-M1 |
 | fackLeftMotor   | REV Ultraplanetary Motor | BLM         | CH-M2 |
 | frontLeftMotor  | REV Ultraplanetary Motor | FLM         | CH-M3 |
-| shooterMotor1   | REV Ultraplanetary Motor | SM1         | EH-M0 |
-| shooterMotor2   | REV Ultraplanetary Motor | SM2         | EH-M1 |
+| shooterMotors   | REV Ultraplanetary Motor | SM1         | EH-M0 |
+| shooterMotors   | REV Ultraplanetary Motor | SM2         | EH-M1 |
