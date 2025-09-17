ad7a4468464af0695a4107412b31960bb66c2176
Aaron Portis <aaronjportis@gmail.com>
2025-09-17 16:39:52 +0100

    HardwareMap.md update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/Shooter.java
@@ -10,23 +10,23 @@ import dev.nextftc.hardware.powerable.SetPower;
 public class Shooter implements Subsystem {
     public static Shooter INSTANCE = new Shooter();
     private Shooter(){}
-    private final MotorEx motor1 = new MotorEx("SM1")
+    private final MotorEx shooterMotor1 = new MotorEx("SM1")
             .reversed()
             .brakeMode();
-    private final MotorEx motor2 = new MotorEx("SM2")
+    private final MotorEx shooterMotor2 = new MotorEx("SM2")
             .reversed()
             .brakeMode();
 
     public Command fire = new ParallelGroup(
-            new SetPower(motor1,1),
-            new SetPower(motor2, 1)
+            new SetPower(shooterMotor1,1),
+            new SetPower(shooterMotor2, 1)
     );
     public Command fireHalf = new ParallelGroup(
-            new SetPower(motor1,0.5),
-            new SetPower(motor2, 0.5)
+            new SetPower(shooterMotor1,0.5),
+            new SetPower(shooterMotor2, 0.5)
     );
     public Command stop = new ParallelGroup(
-            new SetPower(motor1,0),
-            new SetPower(motor2, 0)
+            new SetPower(shooterMotor1,0),
+            new SetPower(shooterMotor2, 0)
     );
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.md
@@ -1,6 +1,8 @@
-| Codebase Name   | Part                     | Device Name |Port  |
-|-----------------| ------------------------ |-------------|----- |
-| FrontRightWheel | REV Ultraplanetary Motor | FRW         |CH-M0 |
-| BackRightWheel  | REV Ultraplanetary Motor | BRW         |CH-M1 |
-| BackLeftWheel   | REV Ultraplanetary Motor | BLW         |CH-M2 |
-| FrontLeftWheel  | REV Ultraplanetary Motor | FLW         |CH-M3 |
+| Codebase Name   | Part                     | Device Name | Port  |
+|-----------------| ------------------------ |-------------|-------|
+| frontRightMotor | REV Ultraplanetary Motor | FRM         | CH-M0 |
+| backRightMotor  | REV Ultraplanetary Motor | BRM         | CH-M1 |
+| fackLeftMotor   | REV Ultraplanetary Motor | BLM         | CH-M2 |
+| frontLeftMotor  | REV Ultraplanetary Motor | FLM         | CH-M3 |
+| shooterMotor1   | REV Ultraplanetary Motor | SM1         | EH-M0 |
+| shooterMotor2   | REV Ultraplanetary Motor | SM2         | EH-M1 |
