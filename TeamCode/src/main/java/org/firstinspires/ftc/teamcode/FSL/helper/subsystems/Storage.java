718a698b2f33674d9dc23783e48189a3dfc33dcd
Aaron Portis <aaronjportis@gmail.com>
2025-10-13 22:22:08 +0100

    Started work on motor code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/Storage.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.FSL.helper.subsystems;
 
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.control.PIDFController;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -9,6 +11,8 @@ import org.firstinspires.ftc.teamcode.FSL.helper.colors.Colors;
 
 import java.util.LinkedList;
 
+import dev.nextftc.control.feedback.PIDCoefficients;
+import dev.nextftc.control.feedback.PIDController;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.groups.ParallelGroup;
@@ -18,13 +22,17 @@ import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.impl.CRServoEx;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.positionable.SetPosition;
 import dev.nextftc.hardware.powerable.SetPower;
 
 public class Storage implements Subsystem {
     private Colors searchColor = Colors.NONE; //default
     private ColorSensor colorSensor;
-    private final CRServoEx spinServo = new CRServoEx("SPS");
+    private final MotorEx spinMotor = new MotorEx("SPM");
+    private final PIDController motorController = new PIDController(new PIDCoefficients(0.01, 0, 0.01));
     private final CRServoEx flickServo = new CRServoEx("FLS");
+    private final double encoderTicksPerThirdRev = 46.7;
     private Storage(){}
     public static Storage INSTANCE = new Storage();
 
@@ -34,9 +42,11 @@ public class Storage implements Subsystem {
         colorSensor.enableLed(true);
         setColorNone.schedule();
     }
-    public final Command reload = new SetPower(spinServo, 0.6).requires(this);
+    public final Command reload = new SequentialGroup(
+            new SetPower(spinMotor, motorController.calculate())
+    );
 
-    public final Command stop = new SetPower(spinServo, 0).requires(this);
+    public final Command stop = new SetPower(spinMotor, 0).requires(this);
     public final Command flickBall = new SequentialGroup(
         new SetPower(flickServo, 1),
         new Delay(0.1),
@@ -54,19 +64,12 @@ public class Storage implements Subsystem {
         searchColor = Colors.NONE;
         ActiveOpMode.gamepad2().setLedColor(0,0,0, Gamepad.LED_DURATION_CONTINUOUS);
     }).requires(this);
-
     public final Command setColorGreen = new InstantCommand(() -> {
         searchColor = Colors.GREEN;
         ActiveOpMode.gamepad2().setLedColor(0,255,0, Gamepad.LED_DURATION_CONTINUOUS);
     }).requires(this);
-
     public final Command setColorPurple = new InstantCommand(() -> {
         searchColor = Colors.PURPLE;
         ActiveOpMode.gamepad2().setLedColor(255,0,255, Gamepad.LED_DURATION_CONTINUOUS);
     }).requires(this);
-
-    public final Command tele = new LambdaCommand()
-            .setUpdate(() -> {
-                ActiveOpMode.telemetry().addData("e", true);
-            }).perpetually().requires(this);
 }
