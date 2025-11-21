337e1a7a19f6a8ec9991d16e3ed320aba4e214a0
Aaron Portis <aaronjportis@gmail.com>
2025-11-21 22:07:00 +0000

    Camera swivel updated

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/MotifNumber.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.FSL.helper;
+
+public enum MotifNumber {
+    PPG,
+    PGP,
+    GPP
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/CameraSwivel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/CameraSwivel.java
@@ -2,7 +2,13 @@ package org.firstinspires.ftc.teamcode.FSL.helper.subsystems;
 
 import android.annotation.SuppressLint;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.FSL.helper.MotifNumber;
 import org.firstinspires.ftc.teamcode.FSL.helper.configs.CameraDetectionConfig;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
@@ -10,107 +16,77 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
 
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.utility.InstantCommand;
-import dev.nextftc.core.commands.utility.LambdaCommand;
-import dev.nextftc.core.commands.utility.NullCommand;
-import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.ftc.ActiveOpMode;
-import dev.nextftc.hardware.impl.ServoEx;
-import dev.nextftc.hardware.positionable.SetPosition;
-
-public class CameraSwivel implements Subsystem {
-    public int motifNumber = 1;
-    public static CameraSwivel INSTANCE = new CameraSwivel();
-    private VisionPortal visionPortal;
+public class CameraSwivel{
+    private final Telemetry telemetry;
+    private final VisionPortal visionPortal;
     private final AprilTagProcessor aprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
-    private final ServoEx swivelServo = new ServoEx("SWS");
+    private final Servo swivelServo;
     private boolean lastScannedRight = false;
-
-    @Override
-    public void initialize() {
-        visionPortal = VisionPortal.easyCreateWithDefaults(
-                ActiveOpMode.hardwareMap().get(WebcamName.class, "Webcam 1"), aprilTagProcessor
-        );
+    public CameraSwivel(HardwareMap hm, Telemetry telemetry){
+        swivelServo = hm.get(Servo.class,"SWS");
+        visionPortal = VisionPortal.easyCreateWithDefaults(hm.get(WebcamName.class, "Webcam 1"), aprilTagProcessor);
+        this.telemetry = telemetry;
     }
-    public Command stopStreaming = new InstantCommand(() -> {
-        visionPortal.stopStreaming();
-    }).requires(this);
-
-    public Command resumeStreaming = new InstantCommand(() -> {
-        visionPortal.resumeStreaming();
-    }).requires(this);
+    public void stopStreaming(){visionPortal.stopStreaming();}
+    public void resumeStreaming(){visionPortal.resumeStreaming();}
     @SuppressLint("DefaultLocale")
-    public Command focusOnAprilTag = new LambdaCommand()
-            .setUpdate(() -> {
-                ActiveOpMode.telemetry().addLine("Webcam\n");
+    public void focusOnAprilTag(boolean sendTelemetry) {
+        List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
 
-                List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
-                ActiveOpMode.telemetry().addData("# AprilTags Detected", currentDetections.size());
+        if (sendTelemetry) {
+            telemetry.addLine("Webcam\n");
+            telemetry.addData("# AprilTags Detected", currentDetections.size());
+        }
 
-                // Step through the list of detections and display info for each one.
-                for (AprilTagDetection detection : currentDetections) {
-                    if (detection.id == 20 /*blue, 24 for red*/) {
-                        ActiveOpMode.telemetry().addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                        ActiveOpMode.telemetry().addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                        ActiveOpMode.telemetry().addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                        ActiveOpMode.telemetry().addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
-
-                        evaluateBearing(detection.ftcPose.bearing).schedule();
-                    }
-                    // Add "key" information to telemetry
-                    ActiveOpMode.telemetry().addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
-                    ActiveOpMode.telemetry().addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
-                    ActiveOpMode.telemetry().addLine("RBE = Range, Bearing & Elevation");
-
-                    determineScan().schedule();
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.id == 20 /*blue, 24 for red*/) {
+                if (sendTelemetry) {
+                    telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                    telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                    telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                    telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
                 }
-            })
-            .setStop((interrupted) -> {
-                visionPortal.close();
-            })
-            .setInterruptible(true)
-            .requires(this)
-            .perpetually();
+                evaluateBearing(detection.ftcPose.bearing);
+            }
+            if (sendTelemetry) {
+                telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+                telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+                telemetry.addLine("RBE = Range, Bearing & Elevation");
+            }
+            determineScan();
+        }
+    }
+    public void evaluateBearing(double tx) {
+        if (tx < -CameraDetectionConfig.CENTRALTOLERANCE) { focusRight(); }
+        else if (tx > CameraDetectionConfig.CENTRALTOLERANCE) { focusLeft(); }
+    }
+    public void determineScan () {
+        double pos = swivelServo.getPosition();
 
-    public Command readMotif = new InstantCommand(() -> {
+        if (pos == 1) {
+            lastScannedRight = true;
+            scanLeft();
+        } else if (pos == 0) {
+            lastScannedRight = false;
+            scanRight();
+        }
+
+        if (lastScannedRight) { scanLeft(); }
+        scanRight();
+    }
+    public void focusLeft () { swivelServo.setPosition(swivelServo.getPosition() - CameraDetectionConfig.FOCUSDX); }
+    public void focusRight () { swivelServo.setPosition(swivelServo.getPosition() + CameraDetectionConfig.FOCUSDX); }
+    public void scanLeft () { swivelServo.setPosition(swivelServo.getPosition() - CameraDetectionConfig.SCANDX); }
+    public void scanRight () { swivelServo.setPosition(swivelServo.getPosition() + CameraDetectionConfig.SCANDX); }
+    public MotifNumber readMotif(){
         AprilTagDetection currentDetection = aprilTagProcessor.getDetections().get(0);
         switch(currentDetection.id){
             case 21:
-                motifNumber = 1;
-                break;
+                return MotifNumber.PPG;
             case 22:
-                motifNumber = 2;
-                break;
-            case 23:
-                motifNumber = 3;
-                break;
+                return MotifNumber.PGP;
             default:
-                motifNumber = 0;
+                return MotifNumber.GPP;
         }
-    }).requires(this);
-
-    public Command evaluateBearing(double tx){
-        if(tx < -CameraDetectionConfig.CENTRALTOLERANCE) return focusRight;
-        else if (tx > CameraDetectionConfig.CENTRALTOLERANCE) return focusLeft;
-        else return new NullCommand();
     }
-    public Command determineScan(){
-        if(swivelServo.getPosition() == 1){
-            lastScannedRight = true;
-            return scanRight;
-        }
-        else if(swivelServo.getPosition() == 0){
-            lastScannedRight = false;
-            return scanLeft;
-        }
-
-        if(lastScannedRight) return scanLeft;
-        return scanRight;
-    }
-    public Command focusLeft = new SetPosition(swivelServo, swivelServo.getPosition() - CameraDetectionConfig.FOCUSDX).requires(this);
-    public Command focusRight = new SetPosition(swivelServo, swivelServo.getPosition() + CameraDetectionConfig.FOCUSDX).requires(this);
-    public Command scanLeft = new SetPosition(swivelServo, swivelServo.getPosition() - CameraDetectionConfig.SCANDX).requires(this);
-    public Command scanRight = new SetPosition(swivelServo, swivelServo.getPosition() + CameraDetectionConfig.SCANDX).requires(this);
-
 }
