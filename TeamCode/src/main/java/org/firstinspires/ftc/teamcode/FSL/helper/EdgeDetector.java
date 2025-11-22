5f31b3a969594b8af43c505fcd5598cf69383022
Aaron Portis <aaronjportis@gmail.com>
2025-11-22 20:26:07 +0000

    mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/EdgeDetector.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.FSL.helper;
-
-public class EdgeDetector {
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/MecanumSet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/subsystems/MecanumSet.java
@@ -1,54 +1,63 @@
 package org.firstinspires.ftc.teamcode.FSL.helper.subsystems;
 
-import androidx.annotation.NonNull;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.utility.InstantCommand;
-import dev.nextftc.core.commands.utility.LambdaCommand;
-import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.core.subsystems.SubsystemGroup;
-import dev.nextftc.ftc.Gamepads;
-import dev.nextftc.hardware.controllable.MotorGroup;
-import dev.nextftc.hardware.driving.DriverControlledCommand;
-import dev.nextftc.hardware.driving.HolonomicMode;
-import dev.nextftc.hardware.driving.MecanumDriverControlled;
-import dev.nextftc.hardware.impl.MotorEx;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-public class MecanumSet implements Subsystem {
-    private final MotorGroup mecanumSetMotors = new MotorGroup(
-            new MotorEx("FLW")
-                    .brakeMode(),
-            new MotorEx("FRW")
-                    .brakeMode()
-                    .reversed(),
-            new MotorEx("BLW")
-                    .brakeMode(),
-            new MotorEx("BRW")
-                    .brakeMode()
-                    .reversed()
-    );
-    private MecanumSet(){}
+public class MecanumSet {
+    private static final double MAX_MOTOR_VEL = 2800;
+    private final DcMotorEx frontLeft;
+    private final DcMotorEx frontRight;
+    private final DcMotorEx backRight;
+    private final DcMotorEx backLeft;
+    private final Telemetry telemetry;
+    public MecanumSet(HardwareMap hm, Telemetry telemetry){
+        frontLeft = hm.get(DcMotorEx.class, "FLW");
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-    @Override
-    public void initialize() {
-        setMedium.schedule();
+        frontRight = hm.get(DcMotorEx.class, "FRW");
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backLeft = hm.get(DcMotorEx.class, "BLW");
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        backRight = hm.get(DcMotorEx.class, "BRW");
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        this.telemetry = telemetry;
     }
 
-    public static final MecanumSet INSTANCE = new MecanumSet();
-    public final MecanumDriverControlled driverController = new MecanumDriverControlled(
-            mecanumSetMotors.getLeader(),
-            mecanumSetMotors.getFollowers()[0],
-            mecanumSetMotors.getFollowers()[1],
-            mecanumSetMotors.getFollowers()[2],
-            Gamepads.gamepad1().leftStickY(),
-            Gamepads.gamepad1().leftStickX(),
-            Gamepads.gamepad1().rightStickX()
-    );
+    public void drive(Gamepad gp, double scalar){
+        scalar = Math.max(0, Math.min(1, scalar));
+        double velocity = scalar * frontLeft.getMotorType().getAchieveableMaxTicksPerSecond();
 
-    public Command setSlow = new InstantCommand(() -> driverController.setScalar(0.4)).requires(this);
-    public Command setMedium = new InstantCommand(() -> driverController.setScalar(0.7)).requires(this);
-    public Command setFast = new InstantCommand(() -> driverController.setScalar(1)).requires(this);
+        double y = -gp.left_stick_y;
+        double x = gp.left_stick_x * 1.1;
+        double rx = gp.right_stick_x;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPowerMod = (y + x + rx) / denominator;
+        double backLeftPowerMod = (y - x + rx) / denominator;
+        double frontRightPowerMod = (y - x - rx) / denominator;
+        double backRightPowerMod = (y + x - rx) / denominator;
+
+        frontLeft.setVelocity(frontLeftPowerMod * velocity);
+        backLeft.setVelocity(backLeftPowerMod * velocity);
+        frontRight.setVelocity(frontRightPowerMod * velocity);
+        backRight.setVelocity(backRightPowerMod * velocity);
+    }
+
+    public void sendTelemetry(){
+        telemetry.addLine("DRIVETRAIN\n");
+        telemetry.addData("FRONT LEFT VEL", frontLeft.getVelocity());
+        telemetry.addData("FRONT LEFT VEL", frontLeft.getVelocity());
+        telemetry.addData("FRONT LEFT VEL", frontLeft.getVelocity());
+        telemetry.addData("FRONT LEFT VEL", frontLeft.getVelocity());
+    }
 }
