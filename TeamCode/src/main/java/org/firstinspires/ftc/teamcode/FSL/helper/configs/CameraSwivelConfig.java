e53076607726224baf7a3a615b5858ab1a9cee09
Aaron Portis <aaronjportis@gmail.com>
2026-02-16 10:13:21 +0000

    Gear ratio wrong

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/configs/CameraSwivelConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSL/helper/configs/CameraSwivelConfig.java
@@ -11,7 +11,7 @@ public class CameraSwivelConfig {
     public static double KP = 0.035;
     public static double KI = 0.0;
     public static double KD = 0.004;
-    public static double TICKS_PER_DEGREE = ((double) 24 /128) * (UltraplanetaryMotorConstants.ENCODER_RES / 360);
+    public static double TICKS_PER_DEGREE = ((double) 128/24) * (UltraplanetaryMotorConstants.ENCODER_RES / 360);
     public static double MAX_VEL = 120.0;
     public static double MAX_OFFSET = TICKS_PER_DEGREE * 90;
     public static int CENTRALTOLERANCE = 3;
